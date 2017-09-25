35a95e52fb6bbdcf172b65136defd77435d59f53
bankabt <barnorcsa@gmail.com>
2017-09-25 17:44:31 +0200

    FeatureDetection Started

--- Emgu_Learn_ConsoleApplication3/Program.cs
+++ Emgu_Learn_ConsoleApplication3/Program.cs
@@ -10,32 +10,34 @@ namespace Emgu_Learn_ConsoleApplication3
     {
         static void Main(string[] args)
         {
-            // Version 1:
-            //string win1 = "AblakNeve";
-            //CvInvoke.NamedWindow(win1);
+            ////Version 1:
+            string win1 = "AblakNeve1";
+            string win2 = "AblakNeve2";
+            CvInvoke.NamedWindow(win1);
+            CvInvoke.NamedWindow(win2);
 
-            //Mat imgMat = new Mat(400, 400, Emgu.CV.CvEnum.DepthType.Cv8U, 3);
-            //imgMat.SetTo(new Bgr(255, 0, 0).MCvScalar);
+            Mat imgMat = new Mat(400, 400, Emgu.CV.CvEnum.DepthType.Cv8U, 3);
+            imgMat.SetTo(new Bgr(255, 0, 0).MCvScalar);
 
-            //CvInvoke.PutText(imgMat, "Hello Norbika!!", new Point(100, 100), Emgu.CV.CvEnum.FontFace.HersheyTriplex, 0.5, new Bgr(0, 255, 255).MCvScalar);
+            CvInvoke.PutText(imgMat, "Hello Norbika!!", new Point(100, 100), Emgu.CV.CvEnum.FontFace.HersheyTriplex, 0.5, new Bgr(0, 255, 255).MCvScalar);
 
 
-            //CvInvoke.Imshow(win1, imgMat);
-            //CvInvoke.WaitKey(1000);
-            //CvInvoke.DestroyWindow(win1);
+            CvInvoke.Imshow(win2, imgMat);
+            CvInvoke.WaitKey(3000);
+            CvInvoke.DestroyWindow(win2);
 
 
 
             // Version 2: 
 
-            using (Mat imgMat = new Mat(400, 400, Emgu.CV.CvEnum.DepthType.Cv8U, 3))
-            {
-                imgMat.SetTo(new Bgr(255, 0, 0).MCvScalar);
+            //using (Mat imgMat = new Mat(400, 400, Emgu.CV.CvEnum.DepthType.Cv8U, 3))
+            //{
+            //    imgMat.SetTo(new Bgr(255, 0, 0).MCvScalar);
 
-                CvInvoke.PutText(imgMat, "Hello Norbika!!", new Point(100, 100), Emgu.CV.CvEnum.FontFace.HersheyTriplex, 0.5, new Bgr(0, 255, 255).MCvScalar);
+            //    CvInvoke.PutText(imgMat, "Hello Norbika!!", new Point(100, 100), Emgu.CV.CvEnum.FontFace.HersheyTriplex, 0.5, new Bgr(0, 255, 255).MCvScalar);
 
-                ImageViewer.Show(imgMat, "Kép");
-            }
+            //    ImageViewer.Show(imgMat, "Kép");
+            //}
 
 
             Console.ReadKey();
--- ShapeDetection1/Program.cs
+++ ShapeDetection1/Program.cs
@@ -5,6 +5,8 @@ using Emgu.CV.Structure;
 using Emgu.CV.UI;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Emgu.CV.Util;
+using System.Drawing;
 
 namespace ShapeDetection1
 {
@@ -22,30 +24,39 @@ namespace ShapeDetection1
             Image<Bgr, byte> image = new Image<Bgr, byte>(fileName);
 
 
+            CvInvoke.Imshow("input", image);
+            CvInvoke.WaitKey(1000);
+            CvInvoke.DestroyWindow("input");
             Console.WriteLine($"image reading: {swatch.ElapsedMilliseconds}");
 
 
-            UMat matriksz = new UMat();
+            Mat matriksz = new Mat();
             CvInvoke.CvtColor(image, matriksz, ColorConversion.Bgr2Gray);
+            CvInvoke.Imshow("inputGray", matriksz);
+            CvInvoke.WaitKey(1000);
+            CvInvoke.DestroyWindow("inputGray");
 
-            //ImageViewer.Show(matriksz);
 
             Console.WriteLine($"image conversion: {swatch.ElapsedMilliseconds}");
 
 
-            UMat pyrLe = new UMat();
-            CvInvoke.PyrDown(matriksz, pyrLe, BorderType.Default);
-            CvInvoke.PyrUp(pyrLe, matriksz, BorderType.Default);
+            //Mat pyrLe = new Mat();
+            //CvInvoke.PyrDown(matriksz, pyrLe, BorderType.Default);
+            //CvInvoke.PyrUp(pyrLe, matriksz, BorderType.Default);
+            CvInvoke.GaussianBlur(matriksz, matriksz, new Size(5, 5), 1);
+            CvInvoke.Imshow("GaussinputGray", matriksz);
+            CvInvoke.WaitKey(1000);
+            CvInvoke.DestroyWindow("GaussinputGray");
 
 
             Console.WriteLine($"image pyramid: {swatch.ElapsedMilliseconds}");
 
 
             // circle detection:
-            double cannyThreshold = 90.0;
-            double circleAccumulatorThreshold = 60;
+            double cannyThreshold = 50;
+            double circleAccumulatorThreshold = 50;
 
-            CircleF[] circles = CvInvoke.HoughCircles(matriksz, HoughType.Gradient, 1.0, 20.0, cannyThreshold, circleAccumulatorThreshold, 20);
+            CircleF[] circles = CvInvoke.HoughCircles(matriksz, HoughType.Gradient, 1.0, 10.0, cannyThreshold, circleAccumulatorThreshold, 20);
 
             // show found circles:
             Image<Bgr, byte> circleImage = new Image<Bgr, byte>(matriksz.Cols, matriksz.Rows);
@@ -54,25 +65,23 @@ namespace ShapeDetection1
             {
                 circleImage.Draw(circle, new Bgr(0.0, 0.0, 0.0), 2);
             }
-            string ablakNev2 = "circleAblak2";
-            //CvInvoke.NamedWindow(ablakNev2);
-            //CvInvoke.Imshow(ablakNev2, circleImage);
-            //CvInvoke.WaitKey(2000);
-            //CvInvoke.DestroyWindow(ablakNev2);
+            CvInvoke.Imshow("circles", circleImage);
+            CvInvoke.WaitKey(1000);
+            CvInvoke.DestroyWindow("circles");
 
 
             Console.WriteLine($"circle search: {swatch.ElapsedMilliseconds}");
 
 
-            // rectangle search:
-            double cannyThresholdLinking = 100.0;
+            // line search:
+            cannyThreshold = 100;
+            double cannyThresholdLinking = 200;
             UMat cannyEdges = new UMat();
-            CvInvoke.Canny(matriksz, cannyEdges, cannyThreshold, cannyThresholdLinking, 3);
+            CvInvoke.Canny(matriksz, cannyEdges, cannyThreshold, cannyThresholdLinking, 5);
 
-            //ImageViewer.Show(cannyEdges);
 
             LineSegment2D[] lines;
-            lines = CvInvoke.HoughLinesP(cannyEdges, 1.0, Math.PI / 45.0, 20, 15, 10);
+            lines = CvInvoke.HoughLinesP(cannyEdges, 1.0, Math.PI / 45.0, 10, 30, 10);
 
 
             // show found lines:
@@ -82,12 +91,9 @@ namespace ShapeDetection1
             {
                 lineImage.Draw(line, new Bgr(0.0, 0.0, 0.0), 2);
             }
-            string ablakNev3 = "circleAblak3";
-            //CvInvoke.NamedWindow(ablakNev3);
-            //CvInvoke.Imshow(ablakNev3, lineImage);
-            //CvInvoke.WaitKey(2000);
-            //CvInvoke.DestroyWindow(ablakNev3);
-
+            CvInvoke.Imshow("lines", lineImage);
+            CvInvoke.WaitKey(2000);
+            CvInvoke.DestroyWindow("lines");
 
 
             Console.WriteLine($"lines search: {swatch.ElapsedMilliseconds}");
@@ -97,10 +103,111 @@ namespace ShapeDetection1
             List<Triangle2DF> triangles = new List<Triangle2DF>();
             List<RotatedRect> boxes = new List<RotatedRect>();
 
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+
+                CvInvoke.FindContours(cannyEdges, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+
+                int contourCount = contours.Size;
+
+                for (int i = 0; i < contourCount; i++)
+                {
+
+                    using (VectorOfPoint pointVector = contours[i])
+                    using (VectorOfPoint approxContour = new VectorOfPoint())
+                    {
+                        CvInvoke.ApproxPolyDP(pointVector, approxContour, CvInvoke.ArcLength(pointVector, true) * 0.05, true);
+
+                        Image<Gray, byte> plot2 = new Image<Gray, byte>(image.Width, image.Height);
+                        plot2.Draw(approxContour.ToArray(), new Gray(100.0), 2);
+
+                        //ImageViewer.Show(plot2);
+
+
+
+                        if (CvInvoke.ContourArea(approxContour, false) > 150) //only consider contours with area greater than 250
+                        {
+                            if (approxContour.Size == 3)
+                            {
+                                Point[] points = approxContour.ToArray();
+                                triangles.Add(new Triangle2DF(points[0], points[1], points[2]));
+                            }
+                            else if (approxContour.Size == 4)
+                            {
+                                bool isRectangle = true;
+                                Point[] points = approxContour.ToArray();
+                                LineSegment2D[] edges = PointCollection.PolyLine(points, true);
+
+                                for (int j = 0; j < edges.Length; j++)
+                                {
+                                    double angle = Math.Abs(edges[(j + 1) % edges.Length].GetExteriorAngleDegree(edges[j]));
+                                    if (angle < 80 || angle > 100)
+                                    {
+                                        isRectangle = false;
+                                        break;
+                                    }
+                                }
+
+                                if (isRectangle)
+                                {
+                                    RotatedRect rotRect = CvInvoke.MinAreaRect(approxContour);
+                                    boxes.Add(rotRect);
+                                }
+
+                            }
+
+
+                        }
+
+                    }
+
+
+                }
+
+                if (triangles.Count > 0 || boxes.Count > 0)
+                {
+
+                    var ch1 = new Image<Gray, byte>(image.Width, image.Height);
+                    var ch2 = new Image<Gray, byte>(image.Width, image.Height);
+                    var ch3 = new Image<Gray, byte>(image.Width, image.Height);
+                    var multichImage = new Image<Bgr, byte>(new Image<Gray, byte>[] { ch1, ch2, ch3 });
+
+                    Image<Gray, byte> plot1 = new Image<Gray, byte>(new Image<Gray, byte>[] { ch1, ch2, ch3 });
+                    CvInvoke.cvCopy(image, multichImage, IntPtr.Zero);
+
+                    for (int j = 0; j < circles.Length; j++)
+                    {
+                        multichImage.Draw(new CircleF(circles[j].Center, circles[j].Radius), new Bgr(200.0, 50.0, 50.0), 2);
+                    }
+
+                    for (int j = 0; j < triangles.Count; j++)
+                    {
+
+                        plot1.Draw(triangles[j], new Gray(200.0), 2);
+                    }
+
+                    for (int j = 0; j < boxes.Count; j++)
+                    {
+                        plot1.Draw(boxes[j], new Gray(200.0), 2);
+                    }
+
+                    CvInvoke.Imshow("plot1", plot1);
+                    CvInvoke.WaitKey(2000);
+                    CvInvoke.DestroyWindow("plot1");
+
+                    CvInvoke.Imshow("multichImage", multichImage);
+                    CvInvoke.WaitKey(2000);
+                    CvInvoke.DestroyWindow("multichImage");
 
 
 
 
+
+                }
+
+
+
+            }
 
 
             Console.ReadKey();
